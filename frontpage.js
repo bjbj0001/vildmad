dc04895d4020a1b67de2559ed434824a2e8331ce
BjarneyMaria <bjarneymaria@gmail.com>
2023-09-12 15:56:01 +0200

    nytt

--- frontpage.js
+++ frontpage.js
@@ -1,46 +1,49 @@
-// Fetch and store the JSON data in sessionStorage (if not already stored)
-if (!sessionStorage.getItem("winterData")) {
-  fetch("https://qeeuingoibugjpdgtfvo.supabase.co/rest/v1/vildmad", {
-    method: "GET",
-    headers: {
-      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZXVpbmdvaWJ1Z2pwZGd0ZnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MjQ5NTYsImV4cCI6MjAwOTQwMDk1Nn0.q-87jgmHdS7Hekf5E6WDdQzMiOcBI82j7JROjcFhg3E",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      // Extract the month values for Winter (0, 1, 11)
-      const winterData = data.filter((item) => [0, 1, 11].includes(item.month));
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("seasons");
 
-      // Store the winterData in sessionStorage for access in "season.html"
-      sessionStorage.setItem("winterData", JSON.stringify(winterData));
+const seasonLinks = document.querySelectorAll("a[data-season]");
+
+seasonLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default link behavior
+
+    const selectedSeason = link.getAttribute("data-season");
+
+    fetch("https://qeeuingoibugjpdgtfvo.supabase.co/rest/v1/vildmad", {
+      method: "GET",
+      headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZXVpbmdvaWJ1Z2pwZGd0ZnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MjQ5NTYsImV4cCI6MjAwOTQwMDk1Nn0.q-87jgmHdS7Hekf5E6WDdQzMiOcBI82j7JROjcFhg3E",
+      },
     })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
+      .then((res) => res.json())
+      .then((data) => showSeasons(data, selectedSeason));
+  });
+});
+
+function showSeasons(seasons, selectedSeason) {
+  const seasonObjects = seasons.filter((season) => {
+    if (selectedSeason === "winter") {
+      return object.month.includes(11) || season.month.includes(0) || season.month.includes(1);
+    } else if (selectedSeason === "spring") {
+      return season.month.includes(2) || season.month.includes(3) || season.month.includes(4);
+    } else if (selectedSeason === "summer") {
+      return season.month.includes(5) || season.month.includes(6) || season.month.includes(7);
+    } else if (selectedSeason === "autumn") {
+      return season.month.includes(8) || season.month.includes(9) || season.month.includes(10);
+    } else {
+      return true; // Show all seasons if no specific season is selected
+    }
+  });
+
+  // Clear the existing content
+  const mainElement = document.querySelector("main");
+  mainElement.innerHTML = "";
+
+  // Append the filtered season objects
+  seasonObjects.forEach((season) => {
+    const link = document.createElement("a");
+    link.href = `season.html?id=${season.id}`;
+    link.textContent = season.name; // Adjust this based on your data structure
+    mainElement.appendChild(link);
+  });
 }
-
-// Define a function to update the page based on the selected season
-function updatePage(monthValues) {
-  // Construct the URL for "season.html" with the selected month values as query parameters
-  const url = `index.html?months=${monthValues.join(",")}`;
-
-  // Navigate to the URL
-  window.location.href = url;
-}
-
-// Add event listeners to each season link
-document.getElementById("winter").addEventListener("click", function () {
-  updatePage([0, 1, 11]); // Pass the selected month values
-});
-
-document.getElementById("spring").addEventListener("click", function () {
-  updatePage([2, 3, 4]); // Pass the selected month values
-});
-
-document.getElementById("summer").addEventListener("click", function () {
-  updatePage([5, 6, 7]); // Pass the selected month values
-});
-
-document.getElementById("autumn").addEventListener("click", function () {
-  updatePage([8, 9, 10]); // Pass the selected month values
-});
--- object.js
+++ object.js
@@ -1,16 +1,16 @@
 const urlParams = new URLSearchParams(window.location.search);
-const selectedSeason = urlParams.get("season");
+const category = urlParams.get("object");
 
-console.log(selectedSeason);
+const ObjectDetailTemplate = document.querySelector("#ObjectDetailTemplate").content;
 
-fetch("https://qeeuingoibugjpdgtfvo.supabase.co/rest/v1/vildmad", {
+fetch("https://qeeuingoibugjpdgtfvo.supabase.co/rest/v1/vildmad" + id, {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZXVpbmdvaWJ1Z2pwZGd0ZnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MjQ5NTYsImV4cCI6MjAwOTQwMDk1Nn0.q-87jgmHdS7Hekf5E6WDdQzMiOcBI82j7JROjcFhg3E",
   },
 })
   .then((res) => res.json())
-  .then(showObjects);
+  .then((data) => showObjects(data));
 
 function showObjects(objects) {
   //looper og kalder showObject
@@ -24,30 +24,6 @@ function showObject(object) {
   //lav en kopi
   const copy = template.cloneNode(true);
 
-  function showSeasons(object) {
-    //  const seasonObjects = seasons.filter((season) => {
-    if (selectedSeason == "winter") {
-      if (object.month.includes("11") || object.month.includes("0") || object.month.includes("1")) {
-        console.log(object);
-      }
-    }
-    if (selectedSeason == "spring") {
-      if (object.month.includes("2") || object.month.includes("3") || object.month.includes("4")) {
-        console.log(object);
-      }
-    }
-    if (selectedSeason == "summer") {
-      if (object.month.includes("5") || object.month.includes("6") || object.month.includes("7")) {
-        console.log(object);
-      }
-    }
-    if (selectedSeason == "autmn") {
-      if (object.month.includes("8") || object.month.includes("9") || object.month.includes("10")) {
-        console.log(object);
-      }
-    }
-  }
-
   // Update content
   copy.querySelector("h1").textContent = object.title;
   copy.querySelector("h2").textContent = object.type;
@@ -58,7 +34,3 @@ function showObject(object) {
   //
   https: document.querySelector("main").appendChild(copy);
 }
-
-// Clear the existing content
-const mainElement = document.querySelector("main");
-mainElement.innerHTML = "";
